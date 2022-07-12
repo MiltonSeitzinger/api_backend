e7d4a5d300c624035c22387cb970e8d7e3add26f
milton <milton0330@gmail.com>
2022-07-11 23:50:58 -0300

    test de usuarios finalizada

--- tests/user.test.js
+++ tests/user.test.js
@@ -6,19 +6,130 @@ const should = require('should');
 let numbers = "0123456789";
 let caracteres = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let password = '';
+let password2 = '';
+
 for (let i=0; i<10; i++) {
   password +=caracteres.charAt(Math.floor(Math.random()*caracteres.length)); 
+  password2 +=caracteres.charAt(Math.floor(Math.random()*caracteres.length)); 
 } 
+
 let mergeNumber='';
 for (let i=0; i<3; i++) {
   mergeNumber +=numbers.charAt(Math.floor(Math.random()*numbers.length)); 
 } 
+
 let email = `email${mergeNumber}@email.com`;
+let email2 = `email${mergeNumber}bis@email.com`;
 let emailInvalido = `email${mergeNumber}email.com`;
 let user = {
   email: email, 
   password: password
 };
+
+describe('POST /api/users/add_user', () => {
+  it('Probando sin datos en agregar nuevo usuario', done => {
+		request(app)
+			.post('/api/users/add_user')
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('Falta email o password');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Probando sin email en agregar nuevo usuario', done => {
+    let user = {
+      password
+    };
+		request(app)
+			.post('/api/users/add_user')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('Falta email o password');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Probando sin password en agregar nuevo usuario', done => {
+    let user = {
+      email
+    };
+		request(app)
+			.post('/api/users/add_user')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('Falta email o password');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Formato de email inválido en agregar nuevo usuario', done => {
+    let user = {
+      email: emailInvalido, 
+      password
+    };
+		request(app)
+			.post('/api/users/add_user')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('Formato de email inválido');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Registrar el email', done => {
+		request(app)
+			.post('/api/users/add_user')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.length.should.equal(167);
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Email ya registrado', done => {
+		request(app)
+			.post('/api/users/add_user')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('El email ya se encuentra registrado');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+});
+
+
+
 /* LOGIN USER */
 describe('POST /api/users/login', () => {
   it('Probando sin datos de login', done => {
@@ -36,9 +147,12 @@ describe('POST /api/users/login', () => {
 		});
 	});
   it('Probando sin email en el login', done => {
+    let user = {
+      password
+    };
 		request(app)
 			.post('/api/users/login')
-			.send({ password: password})
+			.send({ user })
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
@@ -51,9 +165,12 @@ describe('POST /api/users/login', () => {
 		});
 	});
   it('Probando sin password en el login', done => {
+    let user = {
+      email
+    };
 		request(app)
 			.post('/api/users/login')
-			.send({ email: email})
+			.send({ user })
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
@@ -68,11 +185,11 @@ describe('POST /api/users/login', () => {
   it('Formato de email inválido en el login', done => {
     let user = {
       email: emailInvalido, 
-      password: password
+      password
     };
 		request(app)
 			.post('/api/users/login')
-			.send({user})
+			.send({ user })
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
@@ -85,6 +202,10 @@ describe('POST /api/users/login', () => {
 		});
 	});
   it('No está registrado el email', done => {
+    let user = {
+      email: email2,
+      password
+    };
 		request(app)
 			.post('/api/users/login')
 			.send({user})
@@ -99,23 +220,40 @@ describe('POST /api/users/login', () => {
 				done();
 		});
 	});
-
-	/* 
-	it('Usuario registrado completo', done => {
+  it('Login correcto', done => {
 		request(app)
-			.post('/api/user/login')
-			.send({ email: 'aaa@gmail.com'})
+			.post('/api/users/login')
+			.send({ user })
 			.set('Accept', 'application/json')
 			.expect('Content-Type', /json/)
 			.expect(200)
 			.end((err, res) => {
-				if (err) return done(err)
-				res.status.should.equal(200)
-				res.body.message.token.length.should.equal(204)
-				done()
-			})
-		})
-	})
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.length.should.equal(167);
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+  it('Login con password incorrecta', done => {
+    user.password = password2;
+		request(app)
+			.post('/api/users/login')
+			.send({ user })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(200)
+			.end((err, res) => {
+				if(err) return done(err);
+				res.status.should.equal(200);
+				res.body.message.should.equal('Contraseña incorrecta');
+				res.body.error.should.equal(false);
+				done();
+		});
+	});
+});
+
+
 describe('Endpoint no existente', () => { 
 	it('No existe el endpoint', done => {
 		let path = '/enpoint/notfound/';
@@ -129,5 +267,4 @@ describe('Endpoint no existente', () => {
       }
 		});
 	});
- */
 });
